ff678f90f0b0851520547f79369801863095ce38
frankRenlf <frank.renlingfeng@gmail.com>
2023-08-14 21:01:16 +0800

    update

--- web_course/process.py
+++ web_course/process.py
@@ -10,18 +10,56 @@
 """
 
 import multiprocessing as mp
+import time
 
 
-def fn_1(q, n):
+def fn_1(n):
+    res = 0
+    for i in range(n):
+        res += i + i ** 2 + i ** 3
+    return res
+
+
+def normal(n):
+    p_arr = [] * n
+    v_arr = [0] * n
+    for i in range(n):
+        v_arr[i] = fn_1(100000)
+    for i in range(n):
+        a = 1
+    for i in range(n):
+        b = 1
+    for i in range(n):
+        b = 1
+
+
+def fn_m(q, n):
     res = 0
     for i in range(n):
         res += i + i ** 2 + i ** 3
     q.put(res)
 
 
-if __name__ == "__main__":
+def multiPro(n):
     q = mp.Queue()
+    p_arr = []
+    v_arr = [0] * n
+    for i in range(n):
+        p_arr.append(mp.Process(target=fn_m, args=(q, 100000)))
+    for i in range(n):
+        p_arr[i].start()
+    for i in range(n):
+        p_arr[i].join()
+    for i in range(n):
+        v_arr[i] = q.get()
 
-    p1 = mp.Process(target=fn_1, args=(q, 1000))
-    p1.start()
-    p1.join()
+
+if __name__ == "__main__":
+    t1_s = time.time()
+    normal(2)
+    t1_e = time.time()
+    print('time cost', t1_e - t1_s, 's')
+    t2_s = time.time()
+    multiPro(2)
+    t2_e = time.time()
+    print('time cost', t2_e - t2_s, 's')
